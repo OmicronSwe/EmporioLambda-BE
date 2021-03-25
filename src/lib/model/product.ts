7d776fc0f0412d125b6b5a58feb1c7de08785edd
Niccolò Mantovani <nicomanto49@gmail.com>
2021-03-25 19:16:27 +0100

    Fix image

--- src/lib/model/product.ts
+++ src/lib/model/product.ts
@@ -20,7 +20,7 @@ class Product {
     this.name = data.name;
     this.description = data.description;
     this.price = data.price ? data.price : 0;
-    this.imageURL = data.image ? data.image : null;
+    this.imageURL = data.imageUrl ? data.imageUrl : null;
     this.category = data.category ? data.category : null;
   }
 
