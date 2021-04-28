b2ae52cfd199597e21f3736e447eeb4b6f8a3def
Niccolò Mantovani <nicomanto49@gmail.com>
2021-04-28 11:24:11 +0200

    Test with another origin

--- src/lib/APIResponses.ts
+++ src/lib/APIResponses.ts
@@ -18,7 +18,7 @@ export const response = ({
 
   if (cors) {
     response.headers = {
-      "Access-Control-Allow-Origin": "http://localhost:3000",
+      "Access-Control-Allow-Origin": "https://www.google.it",
       "Access-Control-Allow-Credentials": true,
     };
   }
