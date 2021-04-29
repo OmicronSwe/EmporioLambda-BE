a164aaaea3e949097c9160249517366bb2a6cbd8
Niccolò Mantovani <nicomanto49@gmail.com>
2021-04-29 09:46:43 +0200

    Re-name cart table correctly

--- src/services/dynamo/tableName.ts
+++ src/services/dynamo/tableName.ts
@@ -2,7 +2,7 @@ const tableName = {
   product: `${process.env.NAMESPACE}-EML-product`,
   category: `${process.env.NAMESPACE}-EML-category`,
   order: `${process.env.NAMESPACE}-EML-order`,
-  cart: `${process.env.NAMESPACE}-EML-cart2`,
+  cart: `${process.env.NAMESPACE}-EML-cart`,
 };
 
 export default tableName;
