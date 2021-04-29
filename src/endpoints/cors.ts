75fc74d52d71623670524b3c46fa55f8db619d78
Niccolò Mantovani <nicomanto49@gmail.com>
2021-04-29 17:09:19 +0200

    Fix test

--- src/endpoints/cors.ts
+++ src/endpoints/cors.ts
@@ -1,8 +1,11 @@
-module.exports.getProduct = (event, context, callback) => {
+module.exports.handler = (event, context, callback) => {
   // Do work to retrieve Product
 
   const response = {
     statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify({
       data: "test cors",
     }),
