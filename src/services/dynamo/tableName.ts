b6d49ddf306a2432727810455c6d705986f534cd
Niccolò Mantovani <nicomanto49@gmail.com>
2021-04-29 10:13:23 +0200

    Rename product table

--- src/services/dynamo/tableName.ts
+++ src/services/dynamo/tableName.ts
@@ -1,5 +1,5 @@
 const tableName = {
-  product: `${process.env.NAMESPACE}-EML-product`,
+  product: `${process.env.NAMESPACE}-EML-product2`,
   category: `${process.env.NAMESPACE}-EML-category`,
   order: `${process.env.NAMESPACE}-EML-order`,
   cart: `${process.env.NAMESPACE}-EML-cart`,
