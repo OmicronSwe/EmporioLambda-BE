3ddd48c8b5e3e19557e5cdeceb96a9d6df4c6e89
Niccolò Mantovani <nicomanto49@gmail.com>
2021-04-29 10:19:08 +0200

    Restore name table product

--- src/services/dynamo/tableName.ts
+++ src/services/dynamo/tableName.ts
@@ -1,5 +1,5 @@
 const tableName = {
-  product: `${process.env.NAMESPACE}-EML-product2`,
+  product: `${process.env.NAMESPACE}-EML-product`,
   category: `${process.env.NAMESPACE}-EML-category`,
   order: `${process.env.NAMESPACE}-EML-order`,
   cart: `${process.env.NAMESPACE}-EML-cart`,
