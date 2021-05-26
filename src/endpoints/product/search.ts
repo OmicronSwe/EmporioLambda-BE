2efb6fd730735857c7965cfa77b13f5c9f7abca2
Niccolò Mantovani <nicomanto49@gmail.com>
2021-05-26 17:24:44 +0200

    Remove console

--- src/endpoints/product/search.ts
+++ src/endpoints/product/search.ts
@@ -14,111 +14,104 @@ import { SearchProductRequest } from "../../model/product/interface";
  * @param  {} event: event passed when lambda is triggered
  */
 export const index: APIGatewayProxyHandler = async (event) => {
+  if (!event.pathParameters) {
+    return badRequest("PathParameters missing");
+  }
+
+  const keys: Array<string> = [];
+  const valueKeys: Array<string> = [];
+  let countElement: number = 0;
+  let countValue: number = 0;
+  let limit: number;
+  let dataSearch: SearchProductRequest;
+
   try {
-    console.log(event); // eslint-disable-line no-console
+    dataSearch = JSON.parse(decodeURI(event.pathParameters.search));
+  } catch (err) {
+    // handle error to parse URI
+    return badRequest("Bad search path form");
+  }
 
-    if (!event.pathParameters) {
-      return badRequest("PathParameters missing");
-    }
+  let filterExpression = "";
 
-    const keys: Array<string> = [];
-    const valueKeys: Array<string> = [];
-    let countElement: number = 0;
-    let countValue: number = 0;
-    let limit: number;
-    let dataSearch: SearchProductRequest;
+  // check if is present name condition
+  if (dataSearch.name) {
+    // search for product name that cointains the string passed by event
+    filterExpression += `contains(#element${countElement}, :Value${countValue}) AND `;
+    keys.push("name");
+    valueKeys.push(dataSearch.name);
+    countElement++;
+    countValue++;
+  }
 
-    try {
-      dataSearch = JSON.parse(decodeURI(event.pathParameters.search));
-    } catch (err) {
-      // handle error to parse URI
-      return badRequest("Bad search path form");
-    }
+  // check if is present price condition
+  if (dataSearch.minprice || dataSearch.maxprice) {
+    keys.push("price");
 
-    let filterExpression = "";
-
-    // check if is present name condition
-    if (dataSearch.name) {
-      // search for product name that cointains the string passed by event
-      filterExpression += `contains(#element${countElement}, :Value${countValue}) AND `;
-      keys.push("name");
-      valueKeys.push(dataSearch.name);
-      countElement++;
+    // search for minprice if is passed from event
+    if (dataSearch.minprice) {
+      filterExpression += `#element${countElement} >= :Value${countValue} AND `;
+      valueKeys.push(dataSearch.minprice);
       countValue++;
     }
 
-    // check if is present price condition
-    if (dataSearch.minprice || dataSearch.maxprice) {
-      keys.push("price");
+    // search for maxprice if is passed from event
+    if (dataSearch.maxprice) {
+      filterExpression += `#element${countElement} <= :Value${countValue} AND `;
+      valueKeys.push(dataSearch.maxprice);
+      countValue++;
+    }
+    countElement++;
+  }
 
-      // search for minprice if is passed from event
-      if (dataSearch.minprice) {
-        filterExpression += `#element${countElement} >= :Value${countValue} AND `;
-        valueKeys.push(dataSearch.minprice);
-        countValue++;
+  // check if is present category condition
+  if (dataSearch.category) {
+    keys.push("category");
+    filterExpression += "(";
+    const categories: Array<string> = dataSearch.category.split(",");
+    let or: string = " OR ";
+
+    for (let index = 0; index < categories.length; index++) {
+      if (index + 1 == categories.length) {
+        or = "";
       }
 
-      // search for maxprice if is passed from event
-      if (dataSearch.maxprice) {
-        filterExpression += `#element${countElement} <= :Value${countValue} AND `;
-        valueKeys.push(dataSearch.maxprice);
-        countValue++;
-      }
-      countElement++;
+      // build condition by category (category == example OR category ==example2)
+      filterExpression += `#element${countElement} = :Value${countValue++}${or}`;
+      valueKeys.push(categories[index]);
+    }
+    filterExpression += ")";
+  } else {
+    // remove final AND from expression if necessary
+    filterExpression = filterExpression.slice(0, -4);
+  }
+
+  // check if limit of result is present
+  if (dataSearch.limit) {
+    limit = dataSearch.limit;
+  }
+
+  // check if is present lastEvaluatedKEy of product (for pagination)
+  if (dataSearch.lastEvaluatedKey) {
+    dataSearch.lastEvaluatedKey = JSON.parse(dataSearch.lastEvaluatedKey);
+  }
+
+  try {
+    const result = await Dynamo.scan(
+      process.env.PRODUCT_TABLE,
+      filterExpression,
+      keys,
+      valueKeys,
+      limit,
+      dataSearch.lastEvaluatedKey
+    );
+
+    if (result.items.length == 0) {
+      return notFound("Products not found");
     }
 
-    // check if is present category condition
-    if (dataSearch.category) {
-      keys.push("category");
-      filterExpression += "(";
-      const categories: Array<string> = dataSearch.category.split(",");
-      let or: string = " OR ";
-
-      for (let index = 0; index < categories.length; index++) {
-        if (index + 1 == categories.length) {
-          or = "";
-        }
-
-        // build condition by category (category == example OR category ==example2)
-        filterExpression += `#element${countElement} = :Value${countValue++}${or}`;
-        valueKeys.push(categories[index]);
-      }
-      filterExpression += ")";
-    } else {
-      // remove final AND from expression if necessary
-      filterExpression = filterExpression.slice(0, -4);
-    }
-
-    // check if limit of result is present
-    if (dataSearch.limit) {
-      limit = dataSearch.limit;
-    }
-
-    // check if is present lastEvaluatedKEy of product (for pagination)
-    if (dataSearch.lastEvaluatedKey) {
-      dataSearch.lastEvaluatedKey = JSON.parse(dataSearch.lastEvaluatedKey);
-    }
-
-    try {
-      const result = await Dynamo.scan(
-        process.env.PRODUCT_TABLE,
-        filterExpression,
-        keys,
-        valueKeys,
-        limit,
-        dataSearch.lastEvaluatedKey
-      );
-
-      if (result.items.length == 0) {
-        return notFound("Products not found");
-      }
-
-      return response({ data: { result } });
-    } catch (error) {
-      return badResponse("Failed to search products");
-    }
+    return response({ data: { result } });
   } catch (error) {
-    console.log(error); // eslint-disable-line no-console
-    return badResponse("error category");
+    return badResponse("Failed to search products");
   }
 };
